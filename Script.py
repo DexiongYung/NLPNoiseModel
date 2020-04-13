9f00120e6007a60d32793ed3f08f69c27d5c1b83
Dylan Yung <YungDexiong@hotmail.com>
2020-04-13 03:13:33 -0700

    new data

--- Script.py
+++ Script.py
@@ -3,6 +3,11 @@ import torch
 
 df = pd.read_csv('Data/mispelled.csv')
 
-df.apply(lambda x: pd.to_numeric(x, errors='coerce')).dropna()
+for i in range(len(df)):
+    sample = int(torch.distributions.Bernoulli(torch.FloatTensor([0.75])).sample())
 
-df.to_csv('Data/mispelled.csv', index=False)
+    if sample == 1:
+        df.iloc[i].Correct = str(df.iloc[i].Correct).capitalize()
+        df.iloc[i].Noised = str(df.iloc[i].Noised).capitalize()
+
+df.to_csv('Data/mispelled2.csv')
